f115a472d656192dacb34a2cd7fb21a3fb2f2735
左程云 <zuochengyun@zuochengyundeMacBook-Pro.local>
2020-08-10 15:31:12 +0800

    modify code

--- src/topinterviewquestions/Problem_0001_TwoSum.java
+++ src/topinterviewquestions/Problem_0001_TwoSum.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 public class Problem_0001_TwoSum {
 
-	
 	public static int[] twoSum(int[] nums, int target) {
 		HashMap<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < nums.length; i++) {
