bf09fec0c412a61f4e40b88b2ab144a8f4c0935f
左程云 <zuochengyun@zuochengyundeMacBook-Pro.local>
2020-08-16 16:59:40 +0800

    class modify

--- src/topinterviewquestions/Problem_0010_RegularExpressionMatching.java
+++ src/topinterviewquestions/Problem_0010_RegularExpressionMatching.java
@@ -2,7 +2,140 @@ package topinterviewquestions;
 
 public class Problem_0010_RegularExpressionMatching {
 
-	public static boolean isMatch(String str, String exp) {
+	public static boolean isMatch1(String s, String p) {
+		if (s == null || p == null) {
+			return false;
+		}
+		char[] str = s.toCharArray();
+		char[] pattern = p.toCharArray();
+		return isValid(str, pattern) && process(str, pattern, 0, 0);
+	}
+
+	public static boolean isValid(char[] str, char[] pattern) {
+		for (char cha : str) {
+			if (cha == '.' || cha == '*') {
+				return false;
+			}
+		}
+		for (int i = 0; i < pattern.length; i++) {
+			if (pattern[i] == '*' && (i == 0 || pattern[i - 1] == '*')) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// str[si.....] 能否被 pattern[pi...] 变出来
+	// 潜台词：pi位置，pattern[pi] != '*'
+	public static boolean process(char[] str, char[] pattern, int si, int pi) {
+		if (si == str.length) { // si越界了
+			if (pi == pattern.length) {
+				return true;
+			}
+			if (pi + 1 < pattern.length && pattern[pi + 1] == '*') {
+				return process(str, pattern, si, pi + 2);
+			}
+			return false;
+		}
+		// si 没越界
+		if (pi == pattern.length) {
+			return si == str.length;
+		}
+		// si 没越界 pi 没越界
+		if (pi + 1 >= pattern.length || pattern[pi + 1] != '*') {
+			return ((str[si] == pattern[pi]) || (pattern[pi] == '.')) && process(str, pattern, si + 1, pi + 1);
+		}
+		// si 没越界 pi 没越界 pi+1 *
+		if (pattern[pi] != '.' && str[si] != pattern[pi]) {
+			return process(str, pattern, si, pi + 2);
+		}
+		if (process(str, pattern, si, pi + 2)) {
+			return true;
+		}
+		while (si < str.length && (str[si] == pattern[pi] || pattern[pi] == '.')) {
+			if (process(str, pattern, si + 1, pi + 2)) {
+				return true;
+			}
+			si++;
+		}
+		return false;
+	}
+
+	public static boolean isMatch2(String s, String p) {
+		if (s == null || p == null) {
+			return false;
+		}
+		char[] str = s.toCharArray();
+		char[] pattern = p.toCharArray();
+		int[][] dp = new int[str.length + 1][pattern.length + 1];
+		for (int si = 0; si <= str.length; si++) {
+			for (int pi = 0; pi <= pattern.length; pi++) {
+				dp[si][pi] = -1;
+			}
+		}
+		// dp[si][pi] == -1
+		// dp[si][pi] == 0 si pi false
+		// dp[si][pi] == 1 si pi true
+		return isValid(str, pattern) && process2(str, pattern, 0, 0, dp);
+	}
+
+	// str[si.....] 能否被 pattern[pi...] 变出来
+	// 潜台词：pi位置，pattern[pi] != '*'
+	public static boolean process2(char[] str, char[] pattern, int si, int pi, int[][] dp) {
+		if (dp[si][pi] != -1) {
+			return dp[si][pi] == 1;
+		}
+		// si pi 这个参数组合第一次算
+
+		if (si == str.length) { // si越界了
+			if (pi == pattern.length) {
+				dp[si][pi] = 1;
+				return true;
+			}
+			// (pi pi+1) pi+2 ....
+			if (pi + 1 < pattern.length && pattern[pi + 1] == '*') {
+				boolean ans = process2(str, pattern, si, pi + 2, dp);
+				dp[si][pi] = ans ? 1 : 0;
+				return ans;
+			}
+			dp[si][pi] = 0;
+			return false;
+		}
+		// si 没越界
+		if (pi == pattern.length) {
+			boolean ans = si == str.length;
+			dp[si][pi] = ans ? 1 : 0;
+			return ans;
+		}
+		// si 没越界 pi 没越界
+		if (pi + 1 >= pattern.length || pattern[pi + 1] != '*') {
+			boolean ans = ((str[si] == pattern[pi]) || (pattern[pi] == '.'))
+					&& process2(str, pattern, si + 1, pi + 1, dp);
+			dp[si][pi] = ans ? 1 : 0;
+			return ans;
+		}
+		// si 没越界 pi 没越界 pi+1 *
+		if (pattern[pi] != '.' && str[si] != pattern[pi]) {
+			boolean ans = process2(str, pattern, si, pi + 2, dp);
+			dp[si][pi] = ans ? 1 : 0;
+			return ans;
+		}
+		if (process2(str, pattern, si, pi + 2, dp)) {
+			dp[si][pi] = 1;
+			return true;
+		}
+		while (si < str.length && (str[si] == pattern[pi] || pattern[pi] == '.')) {
+			if (process2(str, pattern, si + 1, pi + 2, dp)) {
+				dp[si][pi] = 1;
+				return true;
+			}
+			si++;
+		}
+		dp[si][pi] = 0;
+		return false;
+	}
+
+	public static boolean isMatch3(String str, String exp) {
 		if (str == null || exp == null) {
 			return false;
 		}
@@ -32,20 +165,6 @@ public class Problem_0010_RegularExpressionMatching {
 			}
 		}
 		return dp[0][0];
-	}
-
-	public static boolean isValid(char[] s, char[] e) {
-		for (int i = 0; i < s.length; i++) {
-			if (s[i] == '*' || s[i] == '.') {
-				return false;
-			}
-		}
-		for (int i = 0; i < e.length; i++) {
-			if (e[i] == '*' && (i == 0 || e[i - 1] == '*')) {
-				return false;
-			}
-		}
-		return true;
 	}
 
 	public static boolean[][] initDPMap(char[] s, char[] e) {
