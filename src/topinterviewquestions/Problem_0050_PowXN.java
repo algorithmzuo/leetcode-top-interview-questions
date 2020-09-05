85ccc026e6abeccf349d556eafb78411bcac8c81
左程云 <zuochengyun@zuochengyundeMacBook-Pro.local>
2020-09-05 11:19:48 +0800

    modify code on class

--- src/topinterviewquestions/Problem_0050_PowXN.java
+++ src/topinterviewquestions/Problem_0050_PowXN.java
@@ -2,7 +2,7 @@ package topinterviewquestions;
 
 public class Problem_0050_PowXN {
 
-	public static double myPow(double x, int n) {
+	public static double myPow1(double x, int n) {
 		if (n == 0) {
 			return 1D;
 		}
@@ -20,6 +20,39 @@ public class Problem_0050_PowXN {
 			t = t * t;
 		}
 		return n < 0 ? (1D / ans) : ans;
+	}
+
+	public static double myPow2(double x, int n) {
+		if (n == 0) {
+			return 1D;
+		}
+		int pow = Math.abs(n == Integer.MIN_VALUE ? n + 1 : n);
+		double t = x;
+		double ans = 1D;
+		while (pow != 0) {
+			if ((pow & 1) != 0) {
+				ans *= t;
+			}
+			pow >>= 1;
+			t = t * t;
+		}
+		if (n == Integer.MIN_VALUE) {
+			ans *= x;
+		}
+		return n < 0 ? (1D / ans) : ans;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("world shut up!");
+		int a = Integer.MIN_VALUE;
+		int b = -a;
+		System.out.println(b);
+		double test = 1.0000001D;
+		System.out.println(test == 1D);
+		System.out.println(Math.pow(test, (double) Integer.MIN_VALUE));
+		System.out.println(myPow1(test, Integer.MIN_VALUE));
+		System.out.println(myPow2(test, Integer.MIN_VALUE));
+
 	}
 
 }
