e4c4f8689d17ce8befc1b2b2374e7d157611e1b1
左程云 <algorithmzuo@163.com>
2020-09-17 00:06:58 +0800

    modify code

--- src/topinterviewquestions/Problem_0204_CountPrimes.java
+++ src/topinterviewquestions/Problem_0204_CountPrimes.java
@@ -12,29 +12,12 @@ public class Problem_0204_CountPrimes {
 			if (f[i]) {
 				continue;
 			}
-
-			// 1
-//			int cur = i;
-//			int j = i;
-//			int p = cur * j;
-//			while (p < n) {
-//				if (!f[p]) {
-//					--count;
-//					f[p] = true;
-//				}
-//				cur += 2;
-//				p = j * cur;
-//			}
-			// 1
-
-			// 2
 			for (int j = i * i; j < n; j += 2 * i) {
 				if (!f[j]) {
 					--count;
 					f[j] = true;
 				}
 			}
-			// 2
 		}
 		return count;
 	}
