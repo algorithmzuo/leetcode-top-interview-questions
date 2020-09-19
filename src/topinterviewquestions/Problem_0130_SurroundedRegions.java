197ee16822a455f78b210314348d61f354bcd560
左程云 <algorithmzuo@163.com>
2020-09-20 02:21:45 +0800

    add solve2

--- src/topinterviewquestions/Problem_0130_SurroundedRegions.java
+++ src/topinterviewquestions/Problem_0130_SurroundedRegions.java
@@ -2,7 +2,7 @@ package topinterviewquestions;
 
 public class Problem_0130_SurroundedRegions {
 
-	public static void solve(char[][] board) {
+	public static void solve1(char[][] board) {
 		boolean[] ans = new boolean[1];
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
@@ -50,6 +50,53 @@ public class Problem_0130_SurroundedRegions {
 			change(board, i, j - 1, can);
 			change(board, i, j + 1, can);
 		}
+	}
+
+	// 从边界开始感染的方法，比第一种方法更好
+	public static void solve2(char[][] board) {
+		if (board == null || board.length == 0 || board[0] == null || board[0].length == 0) {
+			return;
+		}
+		int N = board.length;
+		int M = board[0].length;
+		for (int j = 0; j < M; j++) {
+			if (board[0][j] == 'O') {
+				free(board, 0, j);
+			}
+			if (board[N - 1][j] == 'O') {
+				free(board, N - 1, j);
+			}
+		}
+		for (int i = 1; i < N - 1; i++) {
+			if (board[i][0] == 'O') {
+				free(board, i, 0);
+			}
+			if (board[i][M - 1] == 'O') {
+				free(board, i, M - 1);
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (board[i][j] == 'O') {
+					board[i][j] = 'X';
+				}
+				if (board[i][j] == 'F') {
+					board[i][j] = 'O';
+				}
+
+			}
+		}
+	}
+
+	public static void free(char[][] board, int i, int j) {
+		if (i < 0 || i == board.length || j < 0 || j == board[0].length || board[i][j] != 'O') {
+			return;
+		}
+		board[i][j] = 'F';
+		free(board, i + 1, j);
+		free(board, i - 1, j);
+		free(board, i, j + 1);
+		free(board, i, j - 1);
 	}
 
 }
