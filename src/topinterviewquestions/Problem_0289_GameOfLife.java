05cea3fddcefd3803ba1ff5b2fb97bb87af08b8b
左程云 <algorithmzuo@163.com>
2020-09-25 14:13:55 +0800

    modify code

--- src/topinterviewquestions/Problem_0289_GameOfLife.java
+++ src/topinterviewquestions/Problem_0289_GameOfLife.java
@@ -1,5 +1,8 @@
 package topinterviewquestions;
 
+// 有关这个游戏更有意思、更完整的内容：
+// https://www.bilibili.com/video/BV1rJ411n7ri
+// 也推荐这个up主
 public class Problem_0289_GameOfLife {
 
 	public static void gameOfLife(int[][] board) {
