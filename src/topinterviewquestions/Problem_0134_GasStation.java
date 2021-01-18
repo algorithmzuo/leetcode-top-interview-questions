6ba23f75b0f6912b497fe2e0e5fe9609bd416bd3
左程云 <algorithmzuo@163.com>
2021-01-18 17:23:09 +0800

    modify code

--- src/topinterviewquestions/Problem_0134_GasStation.java
+++ src/topinterviewquestions/Problem_0134_GasStation.java
@@ -2,78 +2,80 @@ package topinterviewquestions;
 
 import java.util.LinkedList;
 
+// 本实现比leetcode要求的要难
+// 返回了所有节点是不是良好出发点
 public class Problem_0134_GasStation {
 
-	/*
-	 * 这个方法的时间复杂度O(N)，额外空间复杂度O(N)
-	 */
 	public static int canCompleteCircuit1(int[] gas, int[] cost) {
-		int N = gas.length;
-		int init = -1;
-		for (int i = 0; i < N; i++) {
-			gas[i] -= cost[i];
-			if (gas[i] >= 0) {
-				init = init == -1 ? i : init;
+		boolean[] good = goodArray(gas, cost);
+		for (int i = 0; i < gas.length; i++) {
+			if (good[i]) {
+				return i;
 			}
 		}
-		if (init == -1) {
+		return -1;
+	}
+
+	public static boolean[] goodArray(int[] g, int[] c) {
+		int N = g.length;
+		int M = N << 1;
+		int[] arr = new int[M];
+		for (int i = 0; i < N; i++) {
+			arr[i] = g[i] - c[i];
+			arr[i + N] = g[i] - c[i];
+		}
+		for (int i = 1; i < M; i++) {
+			arr[i] += arr[i - 1];
+		}
+		LinkedList<Integer> w = new LinkedList<>();
+		for (int i = 0; i < N; i++) {
+			while (!w.isEmpty() && arr[w.peekLast()] >= arr[i]) {
+				w.pollLast();
+			}
+			w.addLast(i);
+		}
+		boolean[] ans = new boolean[N];
+		for (int offset = 0, i = 0, j = N; j < M; offset = arr[i++], j++) {
+			if (arr[w.peekFirst()] - offset >= 0) {
+				ans[i] = true;
+			}
+			if (w.peekFirst() == i) {
+				w.pollFirst();
+			}
+			while (!w.isEmpty() && arr[w.peekLast()] >= arr[j]) {
+				w.pollLast();
+			}
+			w.addLast(j);
+		}
+		return ans;
+	}
+
+	// 这个方法的时间复杂度O(N)，额外空间复杂度O(1) 训练营讲了
+	public static int canCompleteCircuit2(int[] gas, int[] cost) {
+		if (gas == null || gas.length == 0) {
 			return -1;
 		}
-		int[] all = new int[N << 1];
-		int index = 0;
-		all[index++] = gas[init];
-		int start = (init < N - 1) ? (init + 1) : 0;
-		while (start != init) {
-			all[index++] = gas[start];
-			start = (start < N - 1) ? (start + 1) : 0;
+		if (gas.length == 1) {
+			return gas[0] < cost[0] ? -1 : 0;
 		}
-		for (int i = 0; i < N; i++) {
-			all[i + N] = all[i];
-		}
-		for (int i = 1; i < all.length; i++) {
-			all[i] = all[i] + all[i - 1];
-		}
-		LinkedList<Integer> minQ = new LinkedList<>();
-		for (int i = 0; i < N; i++) {
-			while (!minQ.isEmpty() && all[minQ.peekLast()] >= all[i]) {
-				minQ.pollLast();
-			}
-			minQ.add(i);
-		}
-		if (all[minQ.peekFirst()] >= 0) {
-			return init;
-		}
-		for (int i = 0; i < N; i++) {
-			while (!minQ.isEmpty() && all[minQ.peekLast()] >= all[i + N]) {
-				minQ.pollLast();
-			}
-			minQ.add(i + N);
-			if (minQ.peekFirst() == i) {
-				minQ.peekFirst();
-			}
-			if (all[minQ.peekFirst()] - all[i] >= 0) {
-				return i + init + 1;
+		boolean[] good = stations(cost, gas);
+		for (int i = 0; i < gas.length; i++) {
+			if (good[i]) {
+				return i;
 			}
 		}
 		return -1;
 	}
 
-	/*
-	 * 这个方法的时间复杂度O(N)，额外空间复杂度O(1)
-	 * 训练营讲了
-	 */
-	public static int canCompleteCircuit2(int[] oil, int[] dis) {
-		int init = changeDisArrayGetInit(oil, dis);
-		if (init != -1) {
-			int ans = enlargeArea(dis, init);
-			if (ans != -1) {
-				return ans;
-			}
+	public static boolean[] stations(int[] cost, int[] gas) {
+		if (cost == null || gas == null || cost.length < 2 || cost.length != gas.length) {
+			return null;
 		}
-		return -1;
+		int init = changeDisArrayGetInit(cost, gas);
+		return init == -1 ? new boolean[cost.length] : enlargeArea(cost, init);
 	}
 
-	public static int changeDisArrayGetInit(int[] oil, int[] dis) {
+	public static int changeDisArrayGetInit(int[] dis, int[] oil) {
 		int init = -1;
 		for (int i = 0; i < dis.length; i++) {
 			dis[i] = oil[i] - dis[i];
@@ -84,8 +86,8 @@ public class Problem_0134_GasStation {
 		return init;
 	}
 
-	public static int enlargeArea(int[] dis, int init) {
-		int ans = -1;
+	public static boolean[] enlargeArea(int[] dis, int init) {
+		boolean[] res = new boolean[dis.length];
 		int start = init;
 		int end = nextIndex(init, dis.length);
 		int need = 0;
@@ -107,30 +109,29 @@ public class Problem_0134_GasStation {
 				}
 				// 如果连通区域已经覆盖整个环，当前的start是良好出发点，进入2阶段
 				if (rest >= 0) {
-					ans = Math.min(start, connectGood(dis, lastIndex(start, dis.length), init));
+					res[start] = true;
+					connectGood(dis, lastIndex(start, dis.length), init, res);
 					break;
 				}
 			}
 			start = lastIndex(start, dis.length);
 		} while (start != init);
-		return ans;
+		return res;
 	}
 
 	// 已知start的next方向上有一个良好出发点
 	// start如果可以达到这个良好出发点，那么从start出发一定可以转一圈
-	public static int connectGood(int[] dis, int start, int init) {
-		int ans = dis.length;
+	public static void connectGood(int[] dis, int start, int init, boolean[] res) {
 		int need = 0;
 		while (start != init) {
 			if (dis[start] < need) {
 				need -= dis[start];
 			} else {
-				ans = Math.min(ans, start);
+				res[start] = true;
 				need = 0;
 			}
 			start = lastIndex(start, dis.length);
 		}
-		return ans;
 	}
 
 	public static int lastIndex(int index, int size) {
@@ -140,83 +141,5 @@ public class Problem_0134_GasStation {
 	public static int nextIndex(int index, int size) {
 		return index == size - 1 ? 0 : (index + 1);
 	}
-	
-	
-//  以下的code是返回所有点是不是良好出发点的结果数组
-//	public static boolean[] stations(int[] dis, int[] oil) {
-//		if (dis == null || oil == null || dis.length < 2
-//				|| dis.length != oil.length) {
-//			return null;
-//		}
-//		int init = changeDisArrayGetInit(dis, oil);
-//		return init == -1 ? new boolean[dis.length] : enlargeArea(dis, init);
-//	}
-//
-//	public static int changeDisArrayGetInit(int[] dis, int[] oil) {
-//		int init = -1;
-//		for (int i = 0; i < dis.length; i++) {
-//			dis[i] = oil[i] - dis[i];
-//			if (dis[i] >= 0) {
-//				init = i;
-//			}
-//		}
-//		return init;
-//	}
-//
-//	public static boolean[] enlargeArea(int[] dis, int init) {
-//		boolean[] res = new boolean[dis.length];
-//		int start = init;
-//		int end = nextIndex(init, dis.length);
-//		int need = 0;
-//		int rest = 0;
-//		do {
-//			// 当前来到的start已经在连通区域中，可以确定后续的开始点一定无法转完一圈
-//			if (start != init && start == lastIndex(end, dis.length)) {
-//				break;
-//			}
-//			// 当前来到的start不在连通区域中，就扩充连通区域
-//			if (dis[start] < need) { // 当前start无法接到连通区的头部
-//				need -= dis[start];
-//			} else { // 当前start可以接到连通区的头部，开始扩充连通区域的尾巴
-//				rest += dis[start] - need;
-//				need = 0;
-//				while (rest >= 0 && end != start) {
-//					rest += dis[end];
-//					end = nextIndex(end, dis.length);
-//				}
-//				// 如果连通区域已经覆盖整个环，当前的start是良好出发点，进入2阶段
-//				if (rest >= 0) {
-//					res[start] = true;
-//					connectGood(dis, lastIndex(start, dis.length), init, res);
-//					break;
-//				}
-//			}
-//			start = lastIndex(start, dis.length);
-//		} while (start != init);
-//		return res;
-//	}
-//
-//	// 已知start的next方向上有一个良好出发点
-//	// start如果可以达到这个良好出发点，那么从start出发一定可以转一圈
-//	public static void connectGood(int[] dis, int start, int init, boolean[] res) {
-//		int need = 0;
-//		while (start != init) {
-//			if (dis[start] < need) {
-//				need -= dis[start];
-//			} else {
-//				res[start] = true;
-//				need = 0;
-//			}
-//			start = lastIndex(start, dis.length);
-//		}
-//	}
-//
-//	public static int lastIndex(int index, int size) {
-//		return index == 0 ? (size - 1) : index - 1;
-//	}
-//
-//	public static int nextIndex(int index, int size) {
-//		return index == size - 1 ? 0 : (index + 1);
-//	}
 
 }
