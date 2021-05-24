4f3776863e49920089489e6d8a4d9f79020ae835
algorithmzuo <algorithmzuo@163.com>
2021-05-24 16:23:07 +0800

    modify code

--- src/topinterviewquestions/Problem_0238_ProductOfArrayExceptSelf.java
+++ src/topinterviewquestions/Problem_0238_ProductOfArrayExceptSelf.java
@@ -29,5 +29,21 @@ public class Problem_0238_ProductOfArrayExceptSelf {
 		}
 		return nums;
 	}
+	
+	public int[] productExceptSelf2(int[] nums) {
+		int n = nums.length;
+		int[] ans = new int[n];
+		ans[0] = nums[0];
+		for (int i = 1; i < n; i++) {
+			ans[i] = ans[i - 1] * nums[i];
+		}
+		int right = 1;
+		for (int i = n - 1; i > 0; i--) {
+			ans[i] = ans[i - 1] * right;
+			right *= nums[i];
+		}
+		ans[0] = right;
+		return ans;
+	}
 
 }
