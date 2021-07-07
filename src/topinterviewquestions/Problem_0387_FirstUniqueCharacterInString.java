5c248e96b553a2dbb804373a2597e40ec01afbbc
algorithmzuo <algorithmzuo@163.com>
2021-07-07 13:52:58 +0800

    modify code

--- src/topinterviewquestions/Problem_0387_FirstUniqueCharacterInString.java
+++ src/topinterviewquestions/Problem_0387_FirstUniqueCharacterInString.java
@@ -5,7 +5,7 @@ public class Problem_0387_FirstUniqueCharacterInString {
 	public int firstUniqChar(String s) {
 		char[] str = s.toCharArray();
 		int N = str.length;
-		int count[] = new int[256];
+		int count[] = new int[26];
 		for (int i = 0; i < N; i++) {
 			count[str[i] - 'a']++;
 		}
